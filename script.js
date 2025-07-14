e022a2829d17b8969d615f11a246f160ddb6e0cc
bilispaps <bilispapsdelivery@gmail.com>
2025-07-14 16:27:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -135,3 +135,14 @@ function calculatePrice() {
 }
 
 window.onload = initMap;
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => {
+        console.log('Service Worker registered:', reg);
+      })
+      .catch(err => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+}
